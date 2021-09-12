aff7abefe2a11bb4bc9c2dccdf54f1c1633de5f9
iamShaf <55636938+iamShafniyasaleem@users.noreply.github.com>
2021-09-12 21:33:58 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace User_Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.Surname = new System.Windows.Forms.Label();
+            this.Date_of_Birth = new System.Windows.Forms.Label();
+            this.Status = new System.Windows.Forms.Label();
+            this.NIC_Number = new System.Windows.Forms.Label();
+            this.combo = new System.Windows.Forms.ComboBox();
+            this.Name1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(165, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(444, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "STUDENT REGISTRATION FORM";
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.White;
+            this.Title.Location = new System.Drawing.Point(194, 95);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(38, 20);
+            this.Title.TabIndex = 1;
+            this.Title.Text = "Title";
+            // 
+            // Surname
+            // 
+            this.Surname.AutoSize = true;
+            this.Surname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Surname.ForeColor = System.Drawing.Color.White;
+            this.Surname.Location = new System.Drawing.Point(194, 125);
+            this.Surname.Name = "Surname";
+            this.Surname.Size = new System.Drawing.Size(74, 20);
+            this.Surname.TabIndex = 2;
+            this.Surname.Text = "Surname";
+            // 
+            // Date_of_Birth
+            // 
+            this.Date_of_Birth.AutoSize = true;
+            this.Date_of_Birth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Date_of_Birth.ForeColor = System.Drawing.Color.White;
+            this.Date_of_Birth.Location = new System.Drawing.Point(194, 159);
+            this.Date_of_Birth.Name = "Date_of_Birth";
+            this.Date_of_Birth.Size = new System.Drawing.Size(109, 20);
+            this.Date_of_Birth.TabIndex = 3;
+            this.Date_of_Birth.Text = "Date_of_Birth";
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Status.ForeColor = System.Drawing.Color.White;
+            this.Status.Location = new System.Drawing.Point(194, 190);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(56, 20);
+            this.Status.TabIndex = 4;
+            this.Status.Text = "Status";
+            // 
+            // NIC_Number
+            // 
+            this.NIC_Number.AutoSize = true;
+            this.NIC_Number.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NIC_Number.ForeColor = System.Drawing.Color.White;
+            this.NIC_Number.Location = new System.Drawing.Point(194, 223);
+            this.NIC_Number.Name = "NIC_Number";
+            this.NIC_Number.Size = new System.Drawing.Size(101, 20);
+            this.NIC_Number.TabIndex = 5;
+            this.NIC_Number.Text = "NIC_Number";
+            // 
+            // combo
+            // 
+            this.combo.FormattingEnabled = true;
+            this.combo.Items.AddRange(new object[] {
+            "Mr.",
+            "Miss.",
+            "Mrs."});
+            this.combo.Location = new System.Drawing.Point(386, 94);
+            this.combo.Name = "combo";
+            this.combo.Size = new System.Drawing.Size(200, 21);
+            this.combo.TabIndex = 6;
+            // 
+            // Name1
+            // 
+            this.Name1.Location = new System.Drawing.Point(386, 124);
+            this.Name1.Name = "Name1";
+            this.Name1.Size = new System.Drawing.Size(200, 20);
+            this.Name1.TabIndex = 7;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(385, 157);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(385, 223);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.White;
+            this.radioButton1.Location = new System.Drawing.Point(386, 190);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 17);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.Text = "Married";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.White;
+            this.radioButton2.Location = new System.Drawing.Point(469, 190);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 17);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.Text = "Unmarried";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(198, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.Location = new System.Drawing.Point(301, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button3.Location = new System.Drawing.Point(402, 273);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button4.Location = new System.Drawing.Point(511, 273);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Add";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(124, 324);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(554, 150);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 486);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Name1);
+            this.Controls.Add(this.combo);
+            this.Controls.Add(this.NIC_Number);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.Date_of_Birth);
+            this.Controls.Add(this.Surname);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label Surname;
+        private System.Windows.Forms.Label Date_of_Birth;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.Label NIC_Number;
+        private System.Windows.Forms.ComboBox combo;
+        private System.Windows.Forms.TextBox Name1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace User_Registration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-44S91MQ\\MSSQLSERVER01;Initial Catalog=userDetails;Integrated Security=True");
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string g = "";
+            if (radioButton1.Checked)
+            {
+                g = radioButton1.Text;
+            }
+
+            if (radioButton2.Checked)
+            {
+                g = radioButton2.Text;
+            }
+
+            con.Open();
+            SqlCommand command = new SqlCommand("insert into userTable1 values ('" + combo.Text + "','" + Name1.Text + "', getdate(),'" + g + "','" + textBox1.Text + "') ", con);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Successfully Inserted!");
+            con.Close();
+            BindData();
+        }
+        void BindData()
+        {
+            SqlCommand command = new SqlCommand("select * from userTable1", con);
+            SqlDataAdapter sd = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string g = "";
+            if (radioButton1.Checked)
+            {
+                g = radioButton1.Text;
+            }
+
+            if (radioButton2.Checked)
+            {
+                g = radioButton2.Text;
+            }
+            con.Open();
+            SqlCommand command = new SqlCommand("update userTable1 set Surname= '" +Name1.Text+"',Date_of_Birth= '"+DateTime.Parse(dateTimePicker1.Text)+"', Status='" + g + "' where NIC_Number='"+ textBox1.Text + "'", con);
+            command.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully updated!");
+            BindData();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                
+                    con.Open();
+                    SqlCommand command = new SqlCommand("delete userTable1 where NIC_Number= '" + textBox1.Text + "'", con);
+                    command.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Deleted Successfully!");
+                    BindData();
+                
+            }
+            else
+            {
+                MessageBox.Show("Ooops!");
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            Action<Control.ControlCollection> func = null;
+            func = (controls) =>
+              {
+                  foreach (Control control in controls)
+                  {
+                      if (control is TextBox)
+                      {
+                          (control as TextBox).Clear();
+                      }
+                      else
+                      {
+                          func(control.Controls);
+                      }
+                  }
+              };
+            func(Controls);
+        }
+    }
+}
